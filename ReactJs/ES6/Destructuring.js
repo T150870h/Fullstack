20d5bbf69f6f978037382d92321ba6b8a4599749
Mytyty <nsthanhhung15112001@gmail.com>
2025-10-03 22:07:11 +0700

    continue ES6

--- ReactJs/ES6/Destructuring.js
+++ ReactJs/ES6/Destructuring.js
@@ -1,8 +1,21 @@
-// Destructuring (Phân rã)
+// Destructuring (Phân rã)- Array- dùng ngoặc vuông
 var array = ["Javascript", "PHP", "Ruby"];
 var [a, b, c] = array;
+
+// rest parameters: Lấy phần tử còn lại của mảng
+var [a, ...rest] = array
 var [a, b ,c] = array;
 
 console.log(a,c);
+console.log(rest);
 
-// Rest parameters (Lấy ra phần còn lại)
+var course = {
+    name: "Js",
+    price: 1000,
+    image: "image"
+}
+
+var {name, price, ...newObj} = course;
+console.log(name, price, newObj);
+
+
